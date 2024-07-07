b74f514b49e19fe65cbd145ef04c89939d9e9dc3
Anuj Singh <fitanuj@gmail.com>
2024-07-08 02:50:54 +0530

    learning pthon

---
+++ Python-Course-1/2-two.py
@@ -0,0 +1,4 @@
+print("Hello Anuj Singh")
+# python print program
+print(25+69)
+print(15<55)
---
+++ Python-Course-1/3-three.py
@@ -0,0 +1,7 @@
+anuj=105
+bhai=546
+
+print(anuj)
+print(anuj+bhai)
+print(anuj,bhai)
+print(id(anuj),id(bhai))
