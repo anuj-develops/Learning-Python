cb1c19ebc634bcf5dd845f4ba5a40721db36f5ff
Anuj Singh <fitanuj@gmail.com>
2024-07-14 14:51:46 +0530

    hi

---
+++ Python-Course-1/12-datatype.py
@@ -0,0 +1,33 @@
+a=5
+print(a,type(a))
+
+a=5.5
+print(a,type(a))
+
+a=2+4j
+print(a,type(a))
+
+s='Hello@122'
+print(s,type(s))
+
+s='''
+Anuj
+As it is
+three line
+'''
+print(s)
+
+l=[10,5.3,'anuj']
+print(l,type(l))
+
+tt=(10,22,'Ann')
+print(tt,type(tt))
+
+d={
+    'name':'Anuj',
+    'age_':'22 year'
+}
+print(d,type(d))
+
+s={10,20,30,10}
+print(s,type(s))
---
+++ Python-Course-1/13-input.py
@@ -0,0 +1,5 @@
+a=eval(input("Enter any digit - "))
+b=eval(input("Enter any digit - "))
+
+print(a+b)
+
